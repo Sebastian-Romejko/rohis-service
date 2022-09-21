6f6ecd7953d0ba5a040b68dc3ab65987d869fa26
PickledCucumber1209 <sebastian.romejko@zhp.net.pl>
2022-09-21 23:47:47 +0200

    Refactor

--- src/main/java/rohis/project/Project.java
+++ src/main/java/rohis/project/Project.java
@@ -13,6 +13,8 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "projects")
 @NoArgsConstructor
+@Getter
+@Setter
 public class Project {
     public Project(String name, ScoutGroup scoutGroup, User user, int rohisYear, LocalDate date, String goals, String description, String materials, String file) {
         this.name = name;
@@ -28,59 +30,37 @@ public class Project {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Getter
-    @Setter
     private Long ID;
 
     @Column(name = "name")
-    @Getter
-    @Setter
     private String name;
 
     @ManyToOne
     @JoinColumn(name = "scout_group_id")
-    @Getter
-    @Setter
     private ScoutGroup scoutGroup;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
-    @Getter
-    @Setter
     private User user;
 
     @Column(name = "rohis_year")
-    @Getter
-    @Setter
     private int rohisYear;
 
     @Column(name = "date")
-    @Getter
-    @Setter
     private LocalDate date;
 
     @Column(name = "goals")
-    @Getter
-    @Setter
     private String goals;
 
     @Column(name = "description")
-    @Getter
-    @Setter
     private String description;
 
     @Column(name = "materials")
-    @Getter
-    @Setter
     private String materials;
 
     @Column(name = "file")
-    @Getter
-    @Setter
     private String file;
 
     @Column(name = "status")
-    @Getter
-    @Setter
     private Status status;
 }
